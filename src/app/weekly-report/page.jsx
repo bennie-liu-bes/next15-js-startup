09a12bcf599e926f26174e60c1761c6bc984e792
mingyeh1214 <226178@bes.com.tw>
2025-02-08 09:48:12 +0800

    Update page.jsx

--- src/app/weekly-report/page.jsx
+++ src/app/weekly-report/page.jsx
@@ -1,51 +1,33 @@
+import { tables } from '@/lib/tables'
 import WeeklyReportClient from './components/WeeklyReportClient'
-import { CONFIG } from '@/config-global'
 
 export default function WeeklyReport() {
   return <WeeklyReportClient />
 }
 
-async function getData(ordNo) {
-  if (!ordNo) return null
+export async function generateMetadata({ searchParams }) {
+  const { ORD_NO: ordNo } = await searchParams
+  const defaultSiteName = '工務所'
+  const defaultOrdCh = '工令'
 
-  try {
-    const response = await fetch(`${CONFIG.API_HOST}/api/db?ordNo=${ordNo}`, {
-      // 加入 next 的 fetch 配置
-      next: {
-        revalidate: 0, // 不快取
-      },
-      // 處理 fetch timeout
-      signal: AbortSignal.timeout(5000), // 5 秒 timeout
-    })
-
-    if (!response.ok) {
-      throw new Error('資料獲取失敗')
+  if (!ordNo) {
+    return {
+      title: `${defaultSiteName} - ${defaultOrdCh}`,
     }
-
-    const result = await response.json()
-    return result
-  } catch (error) {
-    console.error('Error fetching data:', error)
-    return null
   }
-}
 
-export async function generateMetadata({ params, searchParams }) {
+  let siteName = defaultSiteName
+  let ordCh = defaultOrdCh
+
   try {
-    const searchParamsValues = await searchParams
-    const { ORD_NO } = searchParamsValues
-    const data = await getData(ORD_NO)
-    const siteName = `${data?.wkMain[0]?.SITE_CNAME} - ${data?.wkMain[0]?.ORD_CH}`
-
-    return {
-      title: siteName,
-      description: '中華工程-工程工務處-工令週報彙整畫面',
-    }
+    const wkMainData = await tables.wkMain.getData(ordNo)
+    siteName = wkMainData[0]?.SITE_CNAME || defaultSiteName
+    ordCh = wkMainData[0]?.ORD_CH || defaultOrdCh
   } catch (error) {
-    console.error('Error generating metadata:', error)
+    console.error('取得工務所名稱失敗:', error)
+  } finally {
     return {
-      title: '工令週報',
-      description: '中華工程-工程工務處-工令週報彙整畫面',
+      title: `${siteName} - ${ordCh}`,
     }
   }
 }
